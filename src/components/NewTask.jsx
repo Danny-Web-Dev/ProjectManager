304c8fdd14fc1c57a50261ea63e8424029b8b480
Dannymizrachi <danny.mizrachi11@gmail.com>
2024-06-01 23:31:55 +0300

    add task validation

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -2,19 +2,35 @@ import { useState } from 'react';
 
 const NewTask = ({ onAdd }) => {
 	const [enteredTask, setEnteredTask] = useState('');
+	const [showError, setShowError] = useState(false);
 
 	const handleChange = (e) => {
+		setShowError(false);
 		setEnteredTask(e.target.value);
 	};
 
 	const handleClick = () => {
+		if (!isTaskValid()) {
+			setShowError(true);
+			return;
+		}
 		onAdd(enteredTask);
 		setEnteredTask('');
 	};
 
+	const isTaskValid = () => {
+		return enteredTask.length > 0;
+	};
+
+	let inputClasses = 'w-64 px-2 py-1 rounded-sm bg-stone-200';
+
+	if (showError) {
+		inputClasses += ' border border-red-500';
+	}
+
 	return (
 		<div className='flex items-center gap-4'>
-			<input className='w-64 px-2 py-1 rounded-sm bg-stone-200' type='text' onChange={handleChange} value={enteredTask} />
+			<input className={inputClasses} type='text' onChange={handleChange} value={enteredTask} />
 			<button className='text-stone-700 hover:text-stone-950' onClick={handleClick}>
 				Add Task
 			</button>
